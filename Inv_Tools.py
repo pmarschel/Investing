eff34a41a1230ff05b743d99d4d7ac10d8ecbb09
petermarschel <pmarschel@gmail.com>
2015-04-08 19:06:57 -0400

    latest

--- Inv_Tools.py
+++ Inv_Tools.py
@@ -17,7 +17,7 @@ class Company:
         self.price = 52.3
         self.shares = 100
 
-        self.initAssets()
+        self.initBS()
 
     def getTicker(self):
         return self.ticker
@@ -39,10 +39,18 @@ class Company:
 
     def initBS(self):
 
+        # Get the balance sheet
         URL_ROOT = "http://finance.yahoo.com/q/bs?s="
         response = requests.get(URL_ROOT + self.getTicker())
         soup = bs4.BeautifulSoup(response.text)
 
+        self.processAssets(soup)
+        self.processDebt(soup)
+
+
+    def processAssets(self, soup):
+
+        # Process the HTML to get total assets
         tot_assets = soup.find_all(name="strong", text=re.compile("Total Assets"))
         target = tot_assets[0]
 
@@ -52,3 +60,17 @@ class Company:
 
         p=re.compile('[0-9]*')
         self.assets = int(''.join(p.findall(cousin.string)))
+
+    def processDebt(self, soup):
+
+        # Process the HTML to get STD and LTD
+        tot_debt = soup.find_all(name="td", text=re.compile("Long Term Debt"))
+        target = tot_debt[0]
+        sib = target.next_sibling
+
+        p=re.compile('[0-9]*')
+        self.debt = int(''.join(p.findall(sib.string)))
+
+        target = tot_debt[1]
+        sib = target.next_sibling
+        self.debt = self.debt + int(''.join(p.findall(sib.string)))
--- TA.py
+++ TA.py
@@ -8,25 +8,15 @@ response = requests.get("http://finance.yahoo.com/q/bs?s=AAPL")
 
 soup = bs4.BeautifulSoup(response.text)
 
-tables = soup.find_all('table')
-table_ind= tables[1]
-print(len(tables))
-
-count = 0
-
-for table in soup.find_all('table'):
-
-    row_count = 1
-    for row in table.find_all('tr'):
-        row_count += 1
-
-    print(str(count) + ' ' + str(row_count))
-    count += 1
-
 tot_assets = soup.find_all(name="strong", text=re.compile("Total Assets"))
+
 target = tot_assets[0]
 
-parent = target.parent
-aunt = parent.next_sibling
-cousin = aunt.contents[1]
-print(cousin.string)
+p=re.compile('[0-9]*')
+
+parent = target.parent.parent
+for node in parent.contents:
+
+    test = ''.join(node.contents[1])
+
+    test2=p.findall(test)
