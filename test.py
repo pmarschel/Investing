b9e5f0d18168bacf7c1b0783bddda37e7d713b61
pmarschel <pmarschel@gmail.com>
2015-04-22 12:58:43 -0400

    Put in simple time test
    Simple time test; instantiating 1 Company object took a mean of 0.88s,
    with stddev of 0.32s (N=200). Next --> create a write to file method in
    the company object (pass it a file object)

--- test.py
+++ test.py
@@ -1,13 +1,28 @@
 __author__ = 'petermarschel'
 
 import Inv_Tools
+import time
+import statistics as stat
 
+
+
+hold=[]
 tick = "MSFT"
 
-TARG = Inv_Tools.Company(tick)
+for i in range(0,200):
+    start_int = time.clock()
+    TARG = Inv_Tools.Company(tick)
+    end_int = time.clock()
+    hold.append(end_int - start_int)
+    print(i)
 
-TARG.printSelf()
+print(round(stat.mean(hold),2))
+print(round(stat.stdev(hold),2))
 
+
+
+
+#TARG.printSelf()
 #print(TARG.AnnualRD)
 #print(TARG.calcRDAsset(5))
 #print(TARG.AnnualRD)
