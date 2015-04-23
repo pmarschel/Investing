4a7673be879b4e67dce72644b8d54a6eb3d81af3
petermarschel <pmarschel@gmail.com>
2015-04-23 18:18:36 -0400

    fixed time check for Mac

--- test.py
+++ test.py
@@ -4,23 +4,24 @@ import Inv_Tools
 import time
 import statistics as stat
 
-
+start = time.time()
 
 hold=[]
 tick = "MSFT"
 
-for i in range(0,200):
-    start_int = time.clock()
+for i in range(0,10):
+    start_int = time.time()
     TARG = Inv_Tools.Company(tick)
-    end_int = time.clock()
+    end_int = time.time()
     hold.append(end_int - start_int)
     print(i)
 
 print(round(stat.mean(hold),2))
 print(round(stat.stdev(hold),2))
 
+end = time.time()
 
-
+print(round(end - start, 2))
 
 #TARG.printSelf()
 #print(TARG.AnnualRD)
